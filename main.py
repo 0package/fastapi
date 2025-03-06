3ffa6db590e5be47bfd74051b617e4d4e30bdf87
raspberrypi <archiveyeong@gmail.com>
2025-03-06 14:06:08 +0000

    edit main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-app = FastAPI()
-app.mount("/", StaticFiles(directory="public", html = True), name="static")
-
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 import requests
