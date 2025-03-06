5634791dde6652e5704ebfe7b8d10e11495a4f8d
raspberrypi <archiveyeong@gmail.com>
2025-03-06 01:43:14 +0000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Cloudtype에서 사용할 Python 버전
+FROM python:3.11
+
+WORKDIR /app
+
+# 필요한 패키지 설치
+COPY requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# FastAPI 서버 실행
+COPY . .
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -3,3 +3,14 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.mount("/", StaticFiles(directory="public", html = True), name="static")
+=======
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+import requests
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+	return {"message": "Hello CloudType, FastAPI on Raspberry Pi!"}
+
